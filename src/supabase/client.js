d8b108935e528de5e9589fa115502c06b09bf2a8
abishektony <139079623+abishektony@users.noreply.github.com>
2025-04-18 19:29:15 -0400

    Revert "initial commit"
    This reverts commit 9b1b3ccc1b64afe2eb7b345c9eeb8bc2b4339875.

--- src/supabase/client.js
+++
@@ -1,5 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://xcbijepxzaugefxlwpla.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhjYmlqZXB4emF1Z2VmeGx3cGxhIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0NDk0MDA4NCwiZXhwIjoyMDYwNTE2MDg0fQ.GsUN-XN9aIMXOyTWAQRnf3j0jQsi_NklQULZw9EDWRI';
-export const supabase = createClient(supabaseUrl, supabaseKey);
